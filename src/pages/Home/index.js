1b88f5eab0e7a6029f8968180e460a669c018b6a
Vini Black <viniibl4ck@gmail.com>
2021-07-10 13:08:50 -0300

    fazendo uma validaçao simples

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 import { useState } from "react";
 import * as S from "./styled";
+import { useHistory } from "react-router-dom";
 
 function App() {
+  const history = useHistory();
   const [usuario, setUsuario] = useState('');
+  const [erro, setErro] = useState(false);
+
   function handlePesquisa() {
     axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => {
       const repositories = response.data;
@@ -12,13 +16,21 @@ function App() {
         repositoriesName.push(repository.name)
       })
       localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName))
-    });
+      setErro(false)
+      history.push('/repositories');
+    })
+      .catch(err => {
+        setErro(true);
+      });
   }
   return (
-    <S.Container>
-      <S.Input type="text" className="usuarioInput" value={usuario} placeholder="Usuário" onChange={e => setUsuario(e.target.value)} />
-      <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
-    </S.Container>
+    <S.HomeContainer>
+      <S.Container>
+        <S.Input type="text" className="usuarioInput" value={usuario} placeholder="Usuário" onChange={e => setUsuario(e.target.value)} />
+        <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
+      </S.Container>
+      {erro ? <S.ErrorMsg>Ocorreu um erro. Tente novamente.</S.ErrorMsg> : ''}
+    </S.HomeContainer>
   );
 }
 
